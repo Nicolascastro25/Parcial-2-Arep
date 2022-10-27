05796e57d8417b67d8b662f8223e68f932d22f3a
Jaime Nicolás Castro Acuña <jaime.nicolas.castro@gmail.com>
2022-10-26 19:53:03 -0500

    Add files via upload

---
+++ ServiceProxy/src/main/java/co/edu/escuelaing/serviceproxy/ServiceProxy.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+package co.edu.escuelaing.serviceproxy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ *
+ * @author jaime.cacuna
+ */
+public class ServiceProxy {
+
+    private static final String USER_AGENT = "Mozilla/5.0";
+    private static final String GET_URL = "http://ec2-3-85-13-172.compute-1.amazonaws.com:4567/espalindromo?";
+
+    
+    public static void main(String[] args) throws IOException {
+        getApi("12344321");
+    }
+    
+    public static void getApi(String value) throws IOException {
+
+        URL obj = new URL(GET_URL+value);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+        con.setRequestMethod("GET");
+        con.setRequestProperty("User-Agent", USER_AGENT);
+
+        //The following invocation perform the connection implicitly before getting the code
+        int responseCode = con.getResponseCode();
+        System.out.println("GET Response Code :: " + responseCode);
+
+        if (responseCode == HttpURLConnection.HTTP_OK) { // success
+            BufferedReader in = new BufferedReader(new InputStreamReader(
+                    con.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+
+            // print result
+            System.out.println(response.toString());
+        } else {
+            System.out.println("GET request not worked");
+        }
+        System.out.println("GET DONE");
+    }
+
+}
